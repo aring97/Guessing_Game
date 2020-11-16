5edf4549c0d601016fa51a2df3d824a03e8f8f47
Austin Ring <aring97@gmail.com>
2020-11-16 11:52:50 -0600

    step 4

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace Guessing_Game
             System.Console.WriteLine("Type the number now:");
             int userGuess;
             int secretNumber=42;
-            for(int i=3; i>=0; i--){
+            for(int i=4; i>0; i--){
+                System.Console.WriteLine($"you have {i} guesses left");
             userGuess=Int32.Parse(Console.ReadLine());
             if(userGuess==secretNumber){
                 System.Console.WriteLine("You guessed correctly.");
+                break;
             }
             else{
                 System.Console.WriteLine("you guessed incorrectly.");
