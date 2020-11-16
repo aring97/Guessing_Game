551d8d9a8a991bd2a9d4717aa2e85cfaf5bd89d3
Austin Ring <aring97@gmail.com>
2020-11-16 11:58:30 -0600

    step 5

--- Program.cs
+++ Program.cs
@@ -7,11 +7,14 @@ namespace Guessing_Game
         static void Main(string[] args)
         {
             Console.WriteLine("Guess the secret number.");
-            System.Console.WriteLine("Type the number now:");
             int userGuess;
-            int secretNumber=42;
+            Random random=new Random();
+            int secretNumber=random.Next(1,100);
+            int usersCurrentUses=0;
             for(int i=4; i>0; i--){
-                System.Console.WriteLine($"you have {i} guesses left");
+                System.Console.WriteLine($"you have guessed {usersCurrentUses} times.");
+                System.Console.WriteLine($"you have {i} guesses left.");
+                System.Console.WriteLine("Type the number now:");
             userGuess=Int32.Parse(Console.ReadLine());
             if(userGuess==secretNumber){
                 System.Console.WriteLine("You guessed correctly.");
