7c4b0eaa4c2c9a6558acef1d7f0c22215648d5f2
Austin Ring <aring97@gmail.com>
2020-11-16 11:48:49 -0600

    step 3

--- Program.cs
+++ Program.cs
@@ -6,16 +6,19 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess the secret number");
+            Console.WriteLine("Guess the secret number.");
             System.Console.WriteLine("Type the number now:");
-            int userGuess=Int32.Parse(Console.ReadLine());
+            int userGuess;
             int secretNumber=42;
+            for(int i=3; i>=0; i--){
+            userGuess=Int32.Parse(Console.ReadLine());
             if(userGuess==secretNumber){
-                System.Console.WriteLine("You guessed correctly. you win");
+                System.Console.WriteLine("You guessed correctly.");
             }
             else{
-                System.Console.WriteLine("you guessed incorrectly. you lose");
+                System.Console.WriteLine("you guessed incorrectly.");
             }
+            };
         }
     }
 }
