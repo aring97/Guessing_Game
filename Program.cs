824e450f8e33fca321af8df8bf1becaaaa4922c5
Austin Ring <aring97@gmail.com>
2020-11-16 12:21:47 -0600

    finished

--- Program.cs
+++ Program.cs
@@ -9,11 +9,15 @@ namespace Guessing_Game
             System.Console.WriteLine("Please select a difficulty: Easy, Medium, Hard");
             string difficulty=Console.ReadLine();
             int i=0;
+            bool cheater=false;
             if(difficulty.ToLower()=="hard"){
                 i=4;
             }
             else if(difficulty.ToLower()=="medium"){
                 i=6;
+            }
+            else if(difficulty.ToLower()=="cheater"){
+                cheater=true;
             }
             else{
                 i=8;
@@ -23,6 +27,25 @@ namespace Guessing_Game
             Random random=new Random();
             int secretNumber=random.Next(1,100);
             int usersCurrentUses=0;
+            if(cheater){
+                while(cheater){
+                    System.Console.WriteLine($"you have guessed {usersCurrentUses} times.");
+                    System.Console.WriteLine($"you have ?????? guesses left.");
+                    System.Console.WriteLine("Type the number now:");
+                    usersCurrentUses++;
+                    userGuess=Int32.Parse(Console.ReadLine());
+                    if(userGuess==secretNumber){
+                        System.Console.WriteLine("You guessed correctly.");
+                        cheater=false;
+                    }
+                    else if(userGuess>secretNumber){
+                        System.Console.WriteLine("your guess was too high.");
+                    }
+                    else{
+                        System.Console.WriteLine("your guess was too low.");
+                    }
+                }
+            }else{
             for(; i>0; i--){
                 System.Console.WriteLine($"you have guessed {usersCurrentUses} times.");
                 System.Console.WriteLine($"you have {i} guesses left.");
@@ -39,6 +62,7 @@ namespace Guessing_Game
             else{
                 System.Console.WriteLine("your guess was too low.");
             }
+            };
             };
         }
     }
